ac2e273dd28418abb90a665c667c5026cc989c59
Kyle Begeman <kylebegeman@gmail.com>
2017-07-10 10:33:19 -0700

    Initial Commit

---
+++ InlineTagController/InlineTagController.h
@@ -0,0 +1,19 @@
+//
+//  InlineTagController.h
+//  InlineTagController
+//
+//  Created by Kyle Begeman on 7/10/17.
+//  Copyright © 2017 Kyle Begeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InlineTagController.
+FOUNDATION_EXPORT double InlineTagControllerVersionNumber;
+
+//! Project version string for InlineTagController.
+FOUNDATION_EXPORT const unsigned char InlineTagControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InlineTagController/PublicHeader.h>
+
+
